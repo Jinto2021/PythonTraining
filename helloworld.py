f4501a648042c0ff0065627a240ca41ddbdc3b81
Jinto2021 <78880395+Jinto2021@users.noreply.github.com>
2021-02-24 16:49:55 -0600

    Update helloworld.py
    unpacking tuple - example added

--- helloworld.py
+++ helloworld.py
@@ -7,3 +7,14 @@ year= 2021
 date_txt="Python training started on " + month + " {1}, {0}."
 display_date=date_txt.format(year,day)
 print(display_date)
+
+# unpacking tuple - example
+# also using Asterix * (since there is not 5 vasriables)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
