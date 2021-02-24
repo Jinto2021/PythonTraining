99dd6c655a8372842c686f6749fdf00c892d6a19
Jinto2021 <78880395+Jinto2021@users.noreply.github.com>
2021-02-24 14:35:37 -0600

    Update helloworld.py
    Combining strings with + and numbers with format method

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
 print("Welcome Python programmer/ beginner!")
+
+day = 24
+month = 'February'
+year= 2021
+
+date_txt="Python training started on " + month + " {}, {}."
+display_date=date_txt.format(day,year)
+print(display_date)
